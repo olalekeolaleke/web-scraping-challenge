1638cb68b5bf690baa35141398fd8fdc1d19e199
Olaleke Sangogade <105587442+olalekeolaleke@users.noreply.github.com>
2022-08-30 19:26:09 +0100

    Adding Changes

--- Red-Planet/Missions_to_Mars.py
+++ Red-Planet/Missions_to_Mars.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[16]:
-
-
 # import Dependencies
 import pandas as pd
 from splinter import Browser
@@ -13,157 +7,55 @@ import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import urllib.request
-
-
-# In[17]:
+import urllib3
 
 
 executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=True)
+browser = Browser('chrome', **executable_path, headless=False)
 
-
-# # NASA Mars News
-
-# In[71]:
-
+redplanet = {}
 
 # Creating url path
 url = 'https://redplanetscience.com/'
-
-
-# In[72]:
-
 
 # Sending HTTP request to the server
 response = requests.get(url)
 browser.visit(url)
 
 
-# In[73]:
-
-
 # Creating Beautiful soup object
 html = browser.html
 soup = bs(html, 'html.parser')
-type(soup)
-
-
-# In[74]:
-
-
-# Printing formatted version of the soup
-#print(soup.prettify())
-
-
-# In[78]:
-
 
 # Printing the entire article
 article = soup.find("div", class_="list_text").text
-print(article)
-
-
-# In[76]:
-
 
 # Printing the News Title
-
-news_title = soup.find('div', class_= "content_title").text
-
-print(f'The news title is "{news_title}"')
-
-
-# In[77]:
-
+news_title = article.find('div', class_= "content_title").text
+redplanet["news_title"] = news_title
 
 # Printing the News Paragraph
-
-news_p = soup.find("div", class_="article_teaser_body").text
-
-print(f'The News paragraph ---- : {news_p}')
+news_p = article.find("div", class_="article_teaser_body").text
+redplanet["news_p"] = news_p
 
 
-# # JPL Mars Space Images—Featured Image
-# 
-
-# In[26]:
-
+# JPL Mars Space Images—Featured Image
+featured_image_url = "https://spaceimages-mars.com/image/featured/mars2.jpg"
+redplanet["featured_image_url"] = featured_image_url
 
 # Setting featured image url
-featured_image_url = "https://spaceimages-mars.com/"
-
-
-# In[27]:
-
-
-# Sending http request to the server
-response = requests.get(featured_image_url)
-browser.visit(featured_image_url)
-
-
-# In[28]:
-
-
-# Creating Beautiful soup object
-html = browser.html
-soup = bs(html, 'html.parser')
-type(soup)
-
-
-# In[29]:
-
-
-# Printing formatted version of the soup
-#print(soup.prettify())
-
-
-# In[40]:
-
-
-# Printing featured image
-
-featured_image_url = https://spaceimages-mars.com/image/featured/mars2.jpg"
-print(featured_image_url)
-
-img = soup.select_one("div.list_image")
-img
-
-
-# # Mars Facts
-
-# In[31]:
-
+img_url = "https://spaceimages-mars.com"
 
 # Setting up url
 mars_fact_url = "https://galaxyfacts-mars.com/"
-
-
-# In[32]:
-
 
 # Visiting url and sending http requests to the server
 browser.visit(mars_fact_url)
 response = requests.get(mars_fact_url)
 
-
-# In[33]:
-
-
 # Creating Beautiful soup object
 html = browser.html
 soup = bs(html, 'html.parser')
-type(soup)
-
-
-# In[34]:
-
-
-# Printing formatted version of the soup
-#print(soup.prettify())
-
-
-# In[125]:
-
 
 # Use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
 mars_data = pd.read_html("https://space-facts.com/mars/")[1]
@@ -171,77 +63,46 @@ mars_data = pd.read_html("https://space-facts.com/mars/")[1]
 df = mars_data
 df.set_index('Mars - Earth Comparison', inplace=True)
 df
-
-
-# In[126]:
-
-
 # Using Pandas to convert the data to a HTML table string.
-html = df.to_html()
-
-
-# # Mars Hemispheres
-
-# In[101]:
+html_table = df.to_html()
 
 
 # Setting up url
-astropedia_url = 'https://marshemispheres.com/'
-
-
-# In[102]:
-
+base_url = 'https://marshemispheres.com/'
 
 # Sending http request to the server
-response = requests.get(astropedia_url)
-browser.visit(astropedia_url)
-
-
-# In[103]:
+response = requests.get(base_url)
+browser.visit(base_url)
 
 
 # Creating Beautiful soup object
 html = browser.html
 soup = bs(html, 'html.parser')
-type(soup)
-
-
-# In[104]:
-
-
-# Printing formatted version of the soup
-#print(soup.prettify())
-
-
-# In[115]:
 
 
 # Saving both the image url string for the full resolution hemisphere image,and the Hemisphere 
 # title containing the hemisphere name
 
-contents = soup.find_all("div", class_="item")
-
 hemisphere_img_urls = []
+
+contents = soup.find_all("div", class_="item")
 
 for content in contents:
     hemisphere = {}
 
     title = content.find("h3").text
     link = content.find("a", class_="itemLink")["href"]
-    hemisphere = url + link
+    hemisphere = base_url + link
     browser.visit(hemisphere)
     hemispherehtml = browser.html
 
-    soup_2 = bs(hemispherehtml, "lxml")
-    image = soup_2.find("img", class_="wide-image")["src"]
-    img_url = url + image
+    soup2 = bs(hemispherehtml, 'html.parser')
+    image = soup2.find("img", class_= "wide-image")["src"]
+    img_url = base_url + image
     hemisphere = {}
 
     hemisphere_img_urls.append({"title":title,"img_url":img_url})
 
-    
-
 
 # Use a Python dictionary to store the data using the keys `img_url` and `title`
 hemisphere_img_urls
-
--- Red-Planet/app.py
+++ Red-Planet/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+import Missions_to_Mars
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -28,13 +28,13 @@ def scrape():
     redplanet = mongo.db.redplanet
 
     # call the scrape function in our scrape_mars file. This will scrape and save to mongo.
-    redplanet_data = scrape_mars.scrape()
+    redplanet_data = Missions_to_Mars.scrape()
 
      # update our redplanet with the data that is being scraped.
     redplanet.update({}, redplanet_data, upsert=True)
 
   # return a message to our page so we know it was successful.   
-    return redirect("/", code=302)
+    return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
